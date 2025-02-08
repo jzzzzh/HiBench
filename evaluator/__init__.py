1cf86092e5d694c2f55c061fb21a016c81ed36fa
Pangjing-Wu <pangjing.wu@outlook.com>
2025-02-08 12:49:16 +0000

    add eval script

---
+++ evaluator/__init__.py
@@ -0,0 +1,5 @@
+from .code import CodeEvaluator
+from .formula import FormulaEvaluator
+from .fundamental import FundamentalEvaluator
+from .json import JsonEvaluator
+from .paper import PaperEvaluator
