a2b3fa4ce467bac822688348224422015a5962e6
zhuohang <1499765600@qq.com>
2025-02-09 22:02:39 +0800

    formula dataset

--- generator/formula/formula_config.py
+++ generator/formula/formula_config.py
@@ -10,15 +10,15 @@ formula_config = {
     "value complexity":{
         "easy": range(1, 10),
         "easy_float": [round(x * 0.01, 2) for x in range(1, 11)],
-        "medium": range(-100, 100),
-        "medium_float": [round(x * 0.01, 2) for x in range(-100, 101)],
-        "hard": range(-1000, 1000),
-        "hard_float": [round(x * 0.01, 2) for x in range(-1000, 1001)],
+        "medium": range(-50, 50),
+        "medium_float": [round(x * 0.01, 2) for x in range(-50, 51)],
+        "hard": range(-100, 100),
+        "hard_float": [round(x * 0.01, 2) for x in range(-100, 101)],
         },
     "length": {
-        "easy": 5,
-        "medium": 10,
-        "hard": 15,
+        "easy": 3,
+        "medium": 6,
+        "hard": 9,
     },
     "dataset_size": 31,
 }
